8fe640abb63c4bdca65d799932a4500a8cea7bc6
Navee090603 <tk.naveen.tk@gmail.com>
2025-11-27 13:17:15 +0530

    deploy

---
+++ DashboardSite1/DashboardSite1/TaskItem.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//namespace DashboardSite1
+//{
+//    public class TaskItem
+//    {
+//        public int Id { get; set; }
+//        public string Name { get; set; }
+//        public string Frequency { get; set; }
+//        public string FolderPath { get; set; }
+//        public string DocPath { get; set; } 
+//    }
+
+//}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DashboardSite1
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Frequency { get; set; }
+        public string FolderPath { get; set; }
+        public string DocPath { get; set; }
+        public string Status { get; set; } // The new Status property
+    }
+}
